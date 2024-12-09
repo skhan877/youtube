b3cd3b89a1b20721a7078612d349419aac36a63e
skhan59bbk <skhan59@dcs.bbk.ac.uk>
2024-12-09 16:12:34 +0000

    updates

--- yt1.py
+++ yt1.py
@@ -7,20 +7,26 @@ with open(creds, "r") as f:
 
 youtube = build("youtube", "v3", developerKey=api_key)
 
-# request = youtube.channels().list(
-#     part="contentDetails",
-#     forUsername="NovaraMedia"
-# )
+content_req = youtube.channels().list(
+    part="contentDetails",
+    forUsername="BenParkes"
+)
 
-# response = request.execute() 
-# print(response)
+content_resp = content_req.execute() 
+playlistId = content_resp['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+# print(playlistId)
 
 
 playlist_req = youtube.playlistItems().list(
     part="snippet",
-    playlistId="UUOzMAa6IhV6uwYQATYG_2kg",
+    playlistId="UUxvOq2gCCMnnQq-XlO68STA",
     maxResults=10
 )
 
 playlist_resp = playlist_req.execute() 
-print(playlist_resp)
+
+keys = ['publishedAt', 'title']
+
+for i in range(len(playlist_resp['items'])):
+    videos = playlist_resp['items'][i]['snippet']
+    print([videos.get(key) for key in keys])
